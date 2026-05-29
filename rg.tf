2ec8bcb3ea93dd1454502f002dd3463e8f1aa226
aman9814 <amansharma441@gmail.com>
2026-05-29 21:37:19 +0530

    Update rg.tf
    add a rg which name tob2

--- rg.tf
+++ rg.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "relate" {
   name     = "tob"
   location = "eastus"
 }
+resource "azurerm_resource_group" "relate2" {
+  name     = "tob2"
+  location = "eastus"
+}
