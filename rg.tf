782877d06864c8ac896fd1469ebbc3cf71fe4f8c
aman9814 <amansharma441@gmail.com>
2026-05-29 20:56:40 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,4 +1,9 @@
 resource "azurerm_resource_group" "relate" {
   name     = "tob"
+  location = "easst us"
+}
+
+resource "azurerm_resource_group" "relate2" {
+  name     = "tob2"
   location = "wesst europe"
 }
